c4b2094f5f409e45b4c4fe272a3f36730133b312
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2021-05-23 14:10:09 +0300

    Add license text

--- fplib/feature.py
+++ fplib/feature.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2020-present, Andrei Yaskovets
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 import numpy as np
 
 from fplib.minutae import MnType
