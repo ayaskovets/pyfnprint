c451c2b142b224a0d4f9308f1a3e6fb08910afb1
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2024-12-25 16:50:05 +0300

    update copyright

--- fplib/minutae.py
+++ fplib/minutae.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020-present, Andrei Yaskovets
+# Copyright (c) 2024, Andrei Yaskovets
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
